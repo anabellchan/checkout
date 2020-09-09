6a434898ff9af54b68de00742fbfe9d437ae9467
anabellchan <anbellchan@gmail.com>
2020-09-08 21:32:25 -0700

    Run server on port 3000

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var parser = require('body-parser');
 var app = express();
 
 app.use(express.static('client/public'));
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
 
-
-console.log('working...');
-console.log('working...');
